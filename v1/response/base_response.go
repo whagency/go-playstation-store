6a8d741ed67a782f1da494b52f03ee33ef443f7e
Alex Titov <titov292@gmail.com>
2023-10-05 12:55:51 +0300

    feat: error response more details

--- v1/response/base_response.go
+++ v1/response/base_response.go
@@ -1,10 +1,23 @@
 package response
 
 type ErrorsList []struct {
-	Message    string `json:"message"`
-	Type       string `json:"type"`
-	StatusCode int    `json:"statusCode"`
-	ApiName    string `json:"apiName"`
+	Message         string          `json:"message"`
+	Type            string          `json:"type"`
+	StatusCode      int             `json:"statusCode"`
+	ApiName         string          `json:"apiName"`
+	ErrorDetail     ErrorDetail     `json:"error"`
+	ErrorExtensions ErrorExtensions `json:"extensions"`
+}
+
+type ErrorDetail struct {
+	ReferenceID string `json:"referenceId"`
+	Message     string `json:"message"`
+	Reason      int    `json:"reason"`
+	Source      string `json:"source"`
+}
+
+type ErrorExtensions struct {
+	ErrorDetail ErrorDetail `json:"error"`
 }
 
 type Response struct {
